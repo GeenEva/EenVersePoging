f92ac9658a2988617f5643cd6f038bdb66c45fe9
VLKNK <yehtaryavuz@gmail.com>
2018-03-23 14:03:42 +0100

    werw

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -2,22 +2,8 @@
 public class MijnVerseJavaPoging {
 
 	public static void main(String[] args) {
-		System.out.println("Uiteindelijk zal het wel goed komen... denk ik");
-<<<<<<< HEAD
-		System.out.println("This is from Evy");
-		System.out.println("This is from Evy agian");
-		System.out.println("This is from Evy agian");
-		System.out.println("This is from Evy agian 4");
-		System.out.println("This is from Evy agian 5");
-		
-		System.out.println("Eva Message 1");
-		System.out.println("hello how are you :D");
-		
 
-=======
-		System.out.println("I'm adding something too");
-		System.out.println("im volkan and im dumb");
->>>>>>> 11c43725b5bf3c986b985e95b368bcd8305400be
+		System.out.println("im the best");
 	}
 
 }
