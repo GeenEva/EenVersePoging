fe0751083b557780462a509d770bb631a189a8cd
VLKNK <yehtaryavuz@gmail.com>
2018-03-23 14:26:32 +0100

    wer

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -3,7 +3,7 @@ public class MijnVerseJavaPoging {
 
 	public static void main(String[] args) {
 		System.out.println("im the smartest programmer in the universe and i hate myself");
-		werwer
+		System.out.println("werwerwerwer");
 	}
 
 }
