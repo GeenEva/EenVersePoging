2f939bd91d7ac9dbed5fabaa68e81899035e10a6
VLKNK <yehtaryavuz@gmail.com>
2018-03-23 14:52:12 +0100

    i added one printline

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -5,5 +5,6 @@ public class MijnVerseJavaPoging {
 		System.out.println("im the smartest programmer in the universe and i hate myself");
 		System.out.println("please dont fuck up the code");
 
+		System.out.println("im writing this code and eva pushed");
 	}
 }
