2f17c9f3ca01b21e415bd9e314c8f7ee7ccfcdcd
evywetch <evywetch@gmail.com>
2018-03-23 12:54:05 +0100

    Message 2 from Evy.

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -4,6 +4,8 @@ public class MijnVerseJavaPoging {
 	public static void main(String[] args) {
 		System.out.println("Uiteindelijk zal het wel goed komen... denk ik");
 		System.out.println("This is from Evy");
+		System.out.println("This is from Evy agian");
+		
 
 	}
 
