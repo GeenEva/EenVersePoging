4e8bac933665c1d258177efd3d9c55712e6ebed4
= <=>
2018-03-23 14:25:37 +0100

    Oh ja eerst committen

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -3,6 +3,7 @@ public class MijnVerseJavaPoging {
 
 	public static void main(String[] args) {
 		System.out.println("im the smartest programmer in the universe and i hate myself");
+		System.out.println("i get swatty armpits");
 	}
 
 }
