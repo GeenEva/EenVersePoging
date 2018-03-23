cdc48d6d8d722e713868d80209798ddfc1059e23
= <=>
2018-03-23 15:54:59 +0100

    heey, eerst ff committen

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -9,11 +9,10 @@ public class MijnVerseJavaPoging {
 		
 		System.out.println("This is  new clone");
 		System.out.println("kan ik een conlict oplossen of maak ik het erger");
-<<<<<<< HEAD
+
 		System.out.println("ik ben dom of github is dom");
-=======
-		
+
 		System.out.println("PRINTY PRINTY PRINTY");
->>>>>>> 955559c470c4a75f96c12ee067c2fc7681900e57
+
 	}
 }
