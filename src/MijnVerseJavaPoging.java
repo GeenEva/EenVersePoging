480b60688aac9a11026dfffe1c03d92d033fbdc3
evywetch <evywetch@gmail.com>
2018-03-23 16:39:31 +0100

    Chose the delete all println.

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -3,5 +3,26 @@ public class MijnVerseJavaPoging {
 
 	public static void main(String[] args) {
 
+
+
+
+		
+
+
+
+		System.out.println("im the smartest programmer in the universe and i hate myself");
+		System.out.println("please dont fuck up the code");
+
+		System.out.println("im writing this code and eva pushed");
+
+		System.out.println("This is  new clone");
+		System.out.println("kan ik een conlict oplossen of maak ik het erger");
+		System.out.println("ik ben dom of github is dom");
+		System.out.println("PRINTY PRINTY PRINTY");
+		
+		
+		
+
+
 	}
 }
