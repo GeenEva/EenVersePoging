a2204a4a0fa6866f9f1eb9730a8a60863ed95b8e
= <=>
2018-03-23 14:33:25 +0100

    this is the best commit ever

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -3,11 +3,9 @@ public class MijnVerseJavaPoging {
 
 	public static void main(String[] args) {
 		System.out.println("im the smartest programmer in the universe and i hate myself");
-<<<<<<< HEAD
+
 		System.out.println("i get swatty armpits");
-=======
-		
->>>>>>> 21a0a6530e87ab05f0839b1b2098997a946fb56b
+
 	}
 
 }
