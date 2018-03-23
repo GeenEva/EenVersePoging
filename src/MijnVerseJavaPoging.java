fc14fbb503db43cc61e79c43c69874b926ed3e94
VLKNK <yehtaryavuz@gmail.com>
2018-03-23 14:24:26 +0100

    werwer

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -3,7 +3,7 @@ public class MijnVerseJavaPoging {
 
 	public static void main(String[] args) {
 		System.out.println("im the smartest programmer in the universe and i hate myself");
-		System.out.println("im such a fatguy that i cant walk, when im done with dinner :D ");
+		werwer
 	}
 
 }
