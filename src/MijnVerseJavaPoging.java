fbb67a7f628523ff9a9876f6b7df4184369c627e
= <=>
2018-03-23 16:18:42 +0100

    grrrrrroeoeoeoe

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -2,6 +2,7 @@
 public class MijnVerseJavaPoging {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
 
 
 
@@ -19,6 +20,17 @@ public class MijnVerseJavaPoging {
 		
 
 
+=======
+		System.out.println("im the smartest programmer in the universe and i hate myself");
+		System.out.println("please dont fuck up the code");
+
+		System.out.println("im writing this code and eva pushed");
+
+		System.out.println("This is  new clone");
+		System.out.println("kan ik een conlict oplossen of maak ik het erger");
+		System.out.println("ik ben dom of github is dom");
+		System.out.println("PRINTY PRINTY PRINTY");
+>>>>>>> parent of b6d97ad... VOL IS VOL
 
 	}
 }
