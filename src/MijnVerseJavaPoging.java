d23ed20dc1e7e12fcd1f809cc78ce806e73d997b
VLKNK <yehtaryavuz@gmail.com>
2018-03-23 14:47:15 +0100

    i fixed

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -1,17 +1,9 @@
 
-
 public class MijnVerseJavaPoging {
 
 	public static void main(String[] args) {
 		System.out.println("im the smartest programmer in the universe and i hate myself");
-		System.out.println("IM SO SMART HAHAHAHA");
-<<<<<<< HEAD
-		System.out.println("It's almost summer!");
-	}	
-=======
-		System.out.println("EVY IS SMARTER HAHAHAHA");
+		System.out.println("please dont fuck up the code");
+
 	}
->>>>>>> 854e5be7d09b6b38d851aba40dbefb15958d1c83
-
 }
-
