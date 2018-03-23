854e5be7d09b6b38d851aba40dbefb15958d1c83
evywetch <evywetch@gmail.com>
2018-03-23 14:42:51 +0100

    Add message.

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -5,6 +5,7 @@ public class MijnVerseJavaPoging {
 	public static void main(String[] args) {
 		System.out.println("im the smartest programmer in the universe and i hate myself");
 		System.out.println("IM SO SMART HAHAHAHA");
+		System.out.println("EVY IS SMARTER HAHAHAHA");
 	}
 
 }
