76595d382edad2e58a66fe1e2143da408f98c8dc
VLKNK <yehtaryavuz@gmail.com>
2018-03-23 14:32:21 +0100

    fixex

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 public class MijnVerseJavaPoging {
 
@@ -8,18 +8,5 @@ public class MijnVerseJavaPoging {
 	}
 
 }
-=======
 
-public class MijnVerseJavaPoging {
 
-	public static void main(String[] args) {
-		System.out.println("im the smartest programmer in the universe and i hate myself");
-<<<<<<< HEAD
-		System.out.println("i get swatty armpits");
-=======
-		
->>>>>>> 21a0a6530e87ab05f0839b1b2098997a946fb56b
-	}
-
-}
->>>>>>> d4ab1d27487a677e77998fb8674a1c35f9cb074c
