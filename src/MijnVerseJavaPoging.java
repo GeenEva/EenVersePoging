828a10731af944e174bdda14ece5d21e0315f88a
= <=>
2018-03-23 14:44:17 +0100

    Eeen commitje van een pitje

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -5,7 +5,8 @@ public class MijnVerseJavaPoging {
 	public static void main(String[] args) {
 		System.out.println("im the smartest programmer in the universe and i hate myself");
 		System.out.println("IM SO SMART HAHAHAHA");
-	}
+		System.out.println("It's almost summer!");
+	}	
 
 }
 
