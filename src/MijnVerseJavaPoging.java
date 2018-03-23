ae2ba5611fe06917396e9278b10abcb2d4c3dafb
VLKNK <yehtaryavuz@gmail.com>
2018-03-23 15:51:59 +0100

    fixed

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -6,14 +6,11 @@ public class MijnVerseJavaPoging {
 		System.out.println("please dont fuck up the code");
 
 		System.out.println("im writing this code and eva pushed");
-		
+
 		System.out.println("This is  new clone");
 		System.out.println("kan ik een conlict oplossen of maak ik het erger");
-<<<<<<< HEAD
 		System.out.println("ik ben dom of github is dom");
-=======
-		
 		System.out.println("PRINTY PRINTY PRINTY");
->>>>>>> 955559c470c4a75f96c12ee067c2fc7681900e57
+
 	}
 }
