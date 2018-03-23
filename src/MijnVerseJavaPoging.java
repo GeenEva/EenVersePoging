f77169357afe997660910c72ec8129b582517787
= <=>
2018-03-23 16:34:34 +0100

    cammat

--- src/MijnVerseJavaPoging.java
+++ src/MijnVerseJavaPoging.java
@@ -5,19 +5,20 @@ public class MijnVerseJavaPoging {
 
 
 
-		System.out.println("im writing this code and eva pushed");
+
 		
 
-		System.out.println("This is after new ");
-		
+
+
+		System.out.println("im the smartest programmer in the universe and i hate myself");
+		System.out.println("please dont fuck up the code");
+
+		System.out.println("im writing this code and eva pushed");
 
 		System.out.println("This is  new clone");
-
-		System.out.println("VOL IS VOL");
-		
-		System.out.println("Add more line");
-		
-
+		System.out.println("kan ik een conlict oplossen of maak ik het erger");
+		System.out.println("ik ben dom of github is dom");
+		System.out.println("PRINTY PRINTY PRINTY");
 
 
 	}
